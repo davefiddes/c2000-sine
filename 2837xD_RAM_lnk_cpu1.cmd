c289f4075b10493e47938affbbe82c34db5614e6
David J. Fiddes <D.J@fiddes.net>
2021-02-11 12:47:33 +0000

    Fix linker scripts to allow for greater code size
    This fixes problems linking for both RAM and FLASH targets
    by adding additional memory regions for constant data. The
    choice of additional memory regions is random and will
    require further refinement.
    
    Tests:
     - Build for both RAM and FLASH targets ensuring that
       memory use allows for futher expansion.

--- 2837xD_RAM_lnk_cpu1.cmd
+++ 2837xD_RAM_lnk_cpu1.cmd
@@ -69,7 +69,7 @@ SECTIONS
    .bss             : > RAMLS5,    PAGE = 1
    .bss:output      : > RAMLS3,    PAGE = 0
    .init_array      : > RAMM0,     PAGE = 0
-   .const           : > RAMLS5,    PAGE = 1
+   .const           : >> RAMGS2 | RAMGS3 |RAMGS4 | RAMGS5,    PAGE = 1
    .data            : > RAMLS5,    PAGE = 1
    .sysmem          : > RAMLS5,    PAGE = 1
 #else
